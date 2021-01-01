69e824f17791125e2cbf80b3cd086cded29a508a
Erin <galaxyfoll1@gmail.com>
2021-01-01 21:03:47 +0800

    text

--- src/pages/color.js
+++ src/pages/color.js
@@ -4,11 +4,20 @@ export default function Color() {
     return (
         <div>
             Color
-            <div class="card-panel teal lighten-2">This is a card panel with a teal lighten-2 class</div>
+            <div className="card-panel teal lighten-2">This is a card panel with a teal lighten-2 class</div>
 
-            <div class="card-panel">
-                <span class="blue-text text-darken-2">This is a card panel with dark blue text</span>
+            <div className="card-panel">
+                <span className="blue-text text-darken-2">This is a card panel with dark blue text</span>
             </div>
+
+            <span className="red-text darken-1">smthing</span>
+            <span className="blue-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
+            <span className="aqua-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
+            <span className="pink-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
+            <span className="orange-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
+            <span className="purple-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
+            <span className="yellow-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
+            
         </div>
     )
 }
--- src/pages/typography.js
+++ src/pages/typography.js
@@ -4,7 +4,22 @@ import M from 'materialize-css'
 export default function Typography() {
     return (
         <div>
-            Typography
+            <h1>Heading1</h1>
+            <h2>Heading2</h2>
+            <h3>Heading3</h3>
+            <h4>Heading4</h4>
+            <h5>Heading5</h5>
+            <h6>Heading6</h6>
+
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when
+
+            <blockquote>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</blockquote>
+        
+            <p className="flow-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</p>
+
+            <p className="left-align">left aligned text</p>
+            <p className="right-align">right aligned text</p>
+            <p className="center-align">center aligned text</p>
         </div>
     )
 }
