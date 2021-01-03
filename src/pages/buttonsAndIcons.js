f5a5ea0e7717f20867cf25d65eac7fbba63e5829
Erin <galaxyfoll1@gmail.com>
2021-01-03 20:33:04 +0800

    First commit

--- src/pages/buttonsAndIcons.js
+++ src/pages/buttonsAndIcons.js
@@ -8,6 +8,33 @@ export default function ButtonsAndIcons() {
             <a className="waves-effect waves-light btn-large">button
                 <i className="material-icons right">cloud</i>
             </a>
+
+            <p className="waves-effect waves-light btn">read more</p>  
+            <p className="waves-effect waves-light btn">read more</p> 
+            <p className="waves-effect waves-light btn">read more</p> 
+            <p className="waves-effect waves-light btn">read more</p> 
+            <p className="waves-effect waves-light btn red-text white">read more</p> 
+            <p className="waves-effect waves-light btn teal-text white">read more</p>
+            <p className="waves-effect waves-light btn green-text white">read more</p>
+            <p className="waves-effect waves-light btn grey-text white">read more</p>
+            <p className="waves-effect waves-light btn purple-text white">read more</p>
+            <i className="material-icons">shopping_cart</i>
+            <i className="material-icons red-text">settings</i>
+            <i className="material-icons blue-text">search</i>
+            <i className="material-icons">stars</i>
+            <i className="material-icons">done</i>
+            <a class="waves-effect waves-light btn-large"><i class="material-icons left">shopping_cart</i>buy now</a>
+            <a class="waves-effect waves-light btn-large"><i class="material-icons right">shopping_cart</i>buy now</a>
+            <a class="waves-effect waves-light btn-large"><i class="material-icons left">shopping_cart</i>buy now</a>
+            <a class="waves-effect waves-light btn-large red">read more</a>
+            <a class="waves-effect waves-light btn-large purple">read more</a>
+            <a class="waves-effect waves-light btn-large orange">read more</a>
+            <a class="waves-effect waves-light btn-large teal">read more</a>
+            <a class="waves-effect waves-light btn-large black">read more</a>
+            <a class="waves-effect waves-light btn-large yellow">read more</a>
+            <a class="waves-effect waves-light btn-large ">BIG</a>
+            <a class="btn-large disabled">Disabled Button</a>
+
         </div>
     )
 }
--- src/pages/color.js
+++ src/pages/color.js
@@ -10,14 +10,32 @@ export default function Color() {
                 <span className="blue-text text-darken-2">This is a card panel with dark blue text</span>
             </div>
 
-            <span className="red-text darken-1">smthing</span>
-            <span className="blue-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
-            <span className="aqua-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
-            <span className="pink-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
-            <span className="orange-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
-            <span className="purple-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
-            <span className="yellow-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span>
+            <p className="red-text darken-1">smthing</p>
+
+            <p className="blue-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</p>
             
+            <p className="aqua-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</p>
+            
+            <p className="pink-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</p>
+            
+            <p className="orange-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</p>
+            
+            <p className="purple-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</p>
+            
+            <p className="yellow-text text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</p>
+            
+            <p><span className="yellow text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span></p>
+        
+            <p><span className="green text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span></p>
+
+            <p><span className="purple text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span></p>
+
+            <p><span className="blue text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span></p>
+
+            <p><span className="orange text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span></p>
+
+            <p><span className="pink text-darken-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when</span></p>
+
         </div>
     )
 }
